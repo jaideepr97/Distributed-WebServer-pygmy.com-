f76be0706095c8147c8088288f4e7bc048470cbe
Aayush Gupta <aayushgupta@umass.edu>
2020-04-06 19:32:25 -0400

    Updated run_pygmy.sh

--- run_pygmy.sh
+++ run_pygmy.sh
@@ -5,4 +5,4 @@ ssh ${user}@elnux3.cs.umass.edu "sh -c 'cd cs677/lab-2-rao-gupta/src; nohup chmo
 ssh ${user}@elnux1.cs.umass.edu "sh -c 'cd cs677/lab-2-rao-gupta/src; nohup chmod +x run_catalogue.sh; nohup ./run_catalogue.sh > /dev/null 2>&1 &'"
 ssh ${user}@elnux2.cs.umass.edu "sh -c 'cd cs677/lab-2-rao-gupta/src; nohup chmod +x run_order.sh; nohup ./run_order.sh > /dev/null 2>&1 &'"
 #ssh aayushgupta@elnux7.cs.umass.edu "sh -c 'cd cs677/lab-2-rao-gupta/tests; nohup chmod +x run_client.sh; nohup ./run_client.sh > /dev/null 2>&1 &'"
-cd tests/ && ./run_client.sh
+cd tests/ && chmod +x run_client.sh && ./run_client.sh
